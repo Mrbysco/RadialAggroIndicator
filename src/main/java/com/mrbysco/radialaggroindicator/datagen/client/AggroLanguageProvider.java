534c39d62ad840339e9e93513806d24d0d2394e0
Mrbysco <Mrbysco@users.noreply.github.com>
2026-02-23 22:22:02 +0100

    Add missing lang keys for config

--- src/main/java/com/mrbysco/radialaggroindicator/datagen/client/AggroLanguageProvider.java
+++ src/main/java/com/mrbysco/radialaggroindicator/datagen/client/AggroLanguageProvider.java
@@ -13,7 +13,22 @@ public class AggroLanguageProvider extends LanguageProvider {
 
 	@Override
 	protected void addTranslations() {
+		addConfig("general", "General", "General Settings");
+		addConfig("initialAggro", "Initial Aggro", "If true the indicator will be shown when a mob first targets the player");
+		addConfig("indicatorDuration", "Indicator Duration", "The duration in ticks that the aggro indicator will be shown for");
+		addConfig("hideInView", "Hide in View", "If true the indicator will be hidden when the entity is within the player's field of view");
+		addConfig("invertBlacklist", "Invert Blacklist", "If true the blacklist will be inverted to a whitelist, meaning only entities in the list will show the indicator");
 
+		addConfig("client", "Client", "Client Settings");
+		addConfig("radiusScale", "Radius Scale", "The scale of the radius of the aggro indicator. Higher values will make the indicator appear further away from the center of the screen");
+		addConfig("symbol", "Symbol", "The symbol used for the aggro indicator. This can be any single character or a string of characters");
+		addConfig("symbolColorRed", "Symbol Color Red", "The red value of the symbol color rgb");
+		addConfig("symbolColorGreen", "Symbol Color Green", "The green value of the symbol color rgb");
+		addConfig("symbolColorBlue", "Symbol Color Blue", "The blue value of the symbol color rgb");
+		addConfig("symbolRotationLock", "Symbol Rotation Lock", "If true the symbol will not rotate to match the direction of the entity, it will always be upright");
+		addConfig("symbolScale", "Symbol Scale", "The scale of the symbol used for the aggro indicator. Higher values will make the symbol larger");
+		addConfig("fadeIn", "Fade In", "If true the indicator will fade in when it appears");
+		addConfig("fadeOut", "Fade Out", "If true the indicator will fade out when it disappears");
 	}
 
 	/**
